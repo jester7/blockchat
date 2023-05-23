fa36c3887b0a5c39cbb20ece41b8a07db6da0f27
Jovan Jester <jovan@jester.cafe>
2023-05-23 06:17:01 -0600

    autoscroll down after new messages

--- packages/client/src/components/MessageArea.tsx
+++ packages/client/src/components/MessageArea.tsx
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Message } from "./Message";
 import { Entity, getComponentValueStrict } from "@latticexyz/recs";
 import { useMUD } from "../MUDContext";
 import { BigNumber } from "ethers";
 
 interface MessageAreaProps {
-  messageIds: Entity[];
+    messageIds: Entity[];
 }
 
 export const MessageArea: React.FC<MessageAreaProps> = ({ messageIds }) => {
     const {
         components: { Messages, Users },
-        systemCalls: { increment, sendMessage, setUsername, setUserInfo },
-      } = useMUD();
+    } = useMUD();
+
+    const messageAreaRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (messageAreaRef.current) {
+            messageAreaRef.current.scrollTop = messageAreaRef.current.scrollHeight;
+        }
+    }, [messageIds]);
 
     return (
-    <div className="message-area">
-      { messageIds.map((id) => {
-          const message = getComponentValueStrict(Messages, id);
-          const sender = message.sender;
-          const senderName = getComponentValueStrict(Users, BigNumber.from(sender).toHexString() as Entity).username;
-          const senderPic = getComponentValueStrict(Users, BigNumber.from(sender).toHexString() as Entity).userPicture;
-          //const datetime = message.datetime;
-          return ( <Message key={id} username={senderName} userPicture={senderPic} message={message.message} />);
-        }) }
-    </div>
-  );
+        <div className="message-area" ref={messageAreaRef}>
+            {messageIds.map((id) => {
+                const message = getComponentValueStrict(Messages, id);
+                const sender = message.sender;
+                const senderName = getComponentValueStrict(Users, BigNumber.from(sender).toHexString() as Entity).username;
+                const senderPic = getComponentValueStrict(Users, BigNumber.from(sender).toHexString() as Entity).userPicture;
+                //const datetime = message.datetime;
+                return (<Message key={id} username={senderName} userPicture={senderPic} message={message.message} />);
+            })}
+        </div>
+    );
 };
